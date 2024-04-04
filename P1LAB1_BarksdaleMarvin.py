cec7c7e2b3f3e2606bb530c048bdbff89872dc96
barksdam4812 <165978756+barksdam4812@users.noreply.github.com>
2024-04-03 23:34:51 -0400

    Add files via upload

---
+++ P1LAB1_BarksdaleMarvin.py
@@ -0,0 +1,24 @@
+#Marvin Barksdale
+#4/3/2024
+#P1LAB1
+#Python Lab1 intro to input and output!
+
+
+
+First_Name = input("Enter your first name:")
+Last_Name = input("Enter your last name:")
+print("Hello,", First_Name, Last_Name,"! Welcome to CTI-110")
+
+
+#ASCII - generated using www.ascii-generator.site
+
+
+print('#  #        ##                            ')
+print('#  #         #                            ')
+print('#  #   ##    #     ##    ##   # #    ##   ')
+print('####  # ##   #    #     #  #  ####  # ##  ')
+print('####  ##     #    #     #  #  #  #  ##    ')
+print('#  #   ##   ###    ##    ##   #  #   ## ')
+
+
+#hopefully I did that correct
