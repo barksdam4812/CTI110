4fbcc649882ce8d4b7059358b3b26a82bd5a9c00
barksdam4812 <165978756+barksdam4812@users.noreply.github.com>
2024-04-22 00:10:16 -0400

    Add files via upload

---
+++ P1HW2_BarksdaleMarvin (2).py
@@ -0,0 +1,61 @@
+# Marvin Barksdale
+# 4/4/2024
+# P1HW2
+# Create a program that does some basic math on numbers that are entered
+
+
+
+print("This program calculates and displays travel expenses")
+print()
+Budget = int(input("Enter Budget:"))
+print()
+Location = input("Enter your travel destination:")
+print()
+Fuel = int(input("How much do you think you will spend on gas?"))
+print()
+Accomodation = int(input("Approximately, how much will you need for accomodation/hotel?"))
+print()
+Food = int(input("Last, how much do  you need for food?"))
+print("----------------------Travel Expenses-------------------")
+print("location:", Location)
+print("Initial Budget:", Budget)
+print()
+print('Fuel:', Fuel)
+print('Accomodation:', Accomodation)
+print('Food:',Food )
+print()
+print("Remaining Balance:",Budget-Fuel-Accomodation-Food)
+print()
+print()
+print()
+
+print('## ##### ######     ###    ##   ##  #######  ####     ')
+print('## ## ##  ##  ##   ## ##   ##   ##   ##   #   ##      ')
+print('   ##     ##  ##  ##   ##  ##   ##   ##       ##      ')
+print('   ##     #####   ##   ##   ## ##    ####     ##      ')
+print('   ##     ## ##   #######   ## ##    ##       ##      ')
+print('   ##     ## ##   ##   ##    ###     ##   #   ##  ##  ')
+print('  ####   #### ##  ##   ##    ###    #######  #######')
+
+
+#I am not too sure about providing Pseudocode but i will give it a try
+
+#inform user of the purpose of program(opening line)
+#use line spacing for a cleaner look
+#prompt user to input budget information
+#   display budget information
+#prompt user to input destination information
+#   display destination information
+#pronpt user to input estimatated fuel cost
+#   display fuel cost infromation to user
+#prontp user for accomodation estimation
+#   display accomodation 
+#prompt user to input food estimation
+#   display food estimation on second line
+#display divider text with verbage Travel expenses. this divides input from output
+#display results
+#utilize correct spacing between sections
+#Budget-Fuel-Accomodation-Food out put to remaining balance
+#display remaining balance on the final screen
+#attempt to write out pseudocode information.
+#place Travel ASCII at the bottom
