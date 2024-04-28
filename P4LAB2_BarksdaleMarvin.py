9054f6381e0db6840ddfbb643f3ba9b36d5368e3
barksdam4812 <165978756+barksdam4812@users.noreply.github.com>
2024-04-28 10:40:38 -0400

    Add files via upload
    NA

---
+++ P4LAB2_BarksdaleMarvin.py
@@ -0,0 +1,36 @@
+#Marvin Barksdale
+#4/28/2024
+#P4LAB2
+#Use while loop and for loop
+
+
+
+'''
+Get interger from user
+Determin if interger is positive or negative
+if number is positive, display multiplication table
+if number is negative, tell user program cannot accept it
+Ask user to run again?
+If yes, run program
+If no, end program
+'''
+
+
+run_again = 'yes'
+
+while run_again != "no":
+    
+    user_num = int(input("Enter an integer:"))
+
+    if user_num >=0:
+        #display multiplication for that value and range (1-12)
+        for item in range(1,13):                 
+            print(f"{user_num} * {item} = {user_num * item}")                
+                        
+    else:
+        print("This program does not handle negative values")
+
+    run_again = input("Would you like to run the program again?")
+
+#Loop has broken. User enterd 'no'
+print("program is ending...")
